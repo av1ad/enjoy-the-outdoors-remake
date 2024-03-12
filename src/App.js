32c46039303f59aa2a2630997fc11d4bb2cdaa1f
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-03-12 15:05:41 -0400

    Removed header file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Header from './components/Header';
 import Footer from './components/Footer';
 import Navigation from './components/Navigation';
 import HomePage from './components/HomePage/HomePage';
@@ -13,7 +12,6 @@ const App = () => {
   return (
     <Router>
       <div className="app">
-        <Header />
         <Navigation />
         <Routes>
           <Route exact path="/" element={<HomePage />} />
