48d0665d9c3c831a418ece74cb415b2fc3a54018
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-03-12 13:36:23 -0400

    Fixed issue where images weren't showing

--- src/components/MountainsPage/MountainDetail.js
+++ src/components/MountainsPage/MountainDetail.js
@@ -5,12 +5,16 @@ const MountainDetail = ({ mountain }) => {
     return <div id="show-results"></div>;
   }
 
-  const imagePath = `../../images/${mountain.img}`;
+  // Use require.context to dynamically import images from the folder
+  const imageContext = require.context('../../images', false, /\.(png|jpe?g|svg)$/);
+
+  // Find the image that matches the mountain name
+  const imageSrc = imageContext(`./${mountain.img}`);
 
   return (
     <div id="show-results" className="mt-5">
       <h2 className="mountain-name">{mountain.name}</h2>
-      <img src={imagePath} alt={mountain.name} className="mountain-img" />
+      <img src={imageSrc} alt={mountain.name} className="mountain-img" />
       <p className="mountain-elevation">Elevation: {mountain.elevation} ft</p>
       <p className="mountain-desc text-wrap">Description: {mountain.desc}</p>
       <p className="mountain-coords">
